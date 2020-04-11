88fd19c3aa93b4f6281e287f17a777a48e1bef4c
mitchelinaju <mitchelinajuo@gmail.com>
2020-04-11 12:33:55 +0100

    second  commit

---
+++ noteapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class  Note(models.Model):
+    text=models.CharField(max_length=120)
+    created=models.DateTimeField(auto_now_add=True)
+    
+    def __str___(self):
+        return self.text
